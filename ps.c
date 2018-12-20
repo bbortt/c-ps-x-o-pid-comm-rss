ba261c1c74e46b659c452c892e866967cfe1a281
bbortt <timon.borter@gmx.ch>
2018-12-20 14:54:04 +0100

    look for subdirectories in /proc.

--- ps.c
+++ ps.c
@@ -1,23 +1,67 @@
 #include <stdio.h>
-#include <sys/types.h>
 #include <dirent.h>
+#include <sys/types.h>
 
 #define DEBUG 1 // 0 for production
 
-int
-main ()
-{
-  DIR *dir;
-  struct dirent *de;
+/**
+ * Global definition of functions
+ */
+int isSubdirectory(struct dirent *dirent);
+int handleProcDirectory(struct dirent *directory);
 
-  #if DEBUG
-  // do some debugging
-  #endif
-  
-  dir = opendir ("/proc");
-  while ( NULL != (de = readdir (dir)))
-    printf ("%s\n", de -> d_name );
-  closedir (dir);
+/**
+ * Main entry point.
+ *
+ * @return 0 on success
+ */
+int main() {
+	DIR *dir;
+	struct dirent *dirent;
 
-  return 0;
+#if DEBUG
+	// do some debugging
+#endif
+
+	if (NULL == (dir = opendir("/proc"))) {
+		perror("Unable to open directory '/proc'!\n");
+		return 1;
+	}
+
+	while (NULL != (dirent = readdir(dir))) {
+		if (0 == isSubdirectory(dirent)) {
+			if (0 != handleProcDirectory(dirent)) {
+				perror("Error while reading proc information from %s!\n");
+				return 1;
+			}
+		}
+	}
+
+	closedir(dir);
+
+	return 0;
+}
+
+/**
+ * Check if this is effectively a sub-directory. Because
+ * DT_DIR matches also . and .. which is the current
+ * respectively parent directory.
+ *
+ * @return 0 if this is a sub-directory
+ */
+int isSubdirectory(struct dirent *dirent) {
+	return DT_DIR == dirent->d_type && "." != dirent->d_name
+			&& ".." != dirent->d_name;
+}
+
+/**
+ * Search through a specific /proc sub-directory. Trying
+ * to detect and print pid, comm and rss.
+ *
+ * @return 0 upon successful read
+ */
+int handleProcDirectory(struct dirent *directory) {
+	printf("directory %s\n", directory->d_name);
+
+	return 0;
 }
